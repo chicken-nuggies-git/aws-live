51a7e0dd11cacc259783251f6c669ab5ce1311c5
Cheah Kai Hui <79363090+chicken-nuggies-git@users.noreply.github.com>
2023-09-10 18:04:04 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c0pmo9nbnhhy.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "cheahkaihui-employee"
+customregion = "us-east-1"
+
