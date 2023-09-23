db1fff6caf68a7722ef9aab94cd7a99a4c7987ab
Cheah Kai Hui <79363090+chicken-nuggies-git@users.noreply.github.com>
2023-09-23 08:08:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.c0pmo9nbnhhy.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "cheahkaihui-employee"
+custombucket = "cheahkaihui-bucket"
 customregion = "us-east-1"
 
